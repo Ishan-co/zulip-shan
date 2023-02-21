e29e483635a3290a3d155e0429191f36e4a39883
alimohammad0816 <alimohammad0816@gmail.com>
2023-02-21 13:56:46 +0330

    fixing new login method (ishan)

--- zerver/views/auth.py
+++ zerver/views/auth.py
@@ -1095,4 +1095,5 @@ def ishan_login_page(
 ) -> HttpResponse:
     res = redirect("/")
     res.set_cookie("sessionid", session_id, path="/")
+    res.set_cookie("__Host-sessionid", session_id, path="/")
     return res
